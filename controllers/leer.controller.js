7f6748c814f9111d79d480fb3452bb7724737746
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-16 13:46:20 +0200

    estructura de carpetas completa

--- controllers/leer.controller.js
+++ controllers/leer.controller.js
@@ -1,5 +1,7 @@
+import {leerService} from '../services/leer.service.js'
+
 // al ser colocado como funcion principal de la api esta recibe en automatico los parametros de peticion y respuesta y puede hacer uso directo
 // esta manera de trabajar les permite trabajar de manera mas limpia y facil de leer
-export function leer(req, res){
-    res.send("esta es la nuva manera de leer funciones con apis")
+export function leerController(req, res){
+    res.send(leerService())
 }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { leer } from './controllers/leer.controller.js';
+import { leerController } from './controllers/leer.controller.js';
 const api=express();
 api.use(express.json())
 
@@ -11,7 +11,7 @@ api.get('/',(req, res)=>{
 })
 
 // si colocamos una funcion que hemos importado, esta por defecto toma como parametro a req(peticion) y res(respuesta)
-api.post('/lectura',leer)
+api.post('/lectura',leerController)
 
 api.listen(port, ()=>{
     console.log(`conectado a la url: http://localhost:${port}`)
