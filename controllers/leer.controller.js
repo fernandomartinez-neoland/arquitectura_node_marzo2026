763cd55ea6c628b1fe0aa45f9448ef6dd1ffb179
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-16 12:56:36 +0200

    comentarios

--- controllers/leer.controller.js
+++ controllers/leer.controller.js
@@ -1,3 +1,5 @@
+// al ser colocado como funcion principal de la api esta recibe en automatico los parametros de peticion y respuesta y puede hacer uso directo
+// esta manera de trabajar les permite trabajar de manera mas limpia y facil de leer
 export function leer(req, res){
     res.send("esta es la nuva manera de leer funciones con apis")
 }
